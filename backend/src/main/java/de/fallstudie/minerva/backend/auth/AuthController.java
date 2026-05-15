a1023707bb3fc5113ce909f60f9c666ce813500d
onehunga <julian@michels.hamburg>
2026-05-15 17:02:02 +0200

    chore(auth): delete old authentication based register

--- backend/src/main/java/de/fallstudie/minerva/backend/auth/AuthController.java
+++ backend/src/main/java/de/fallstudie/minerva/backend/auth/AuthController.java
@@ -1,11 +1,8 @@
 package de.fallstudie.minerva.backend.auth;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +15,6 @@ public class AuthController {
 	@PostMapping("/login")
 	public TokenResponse login(@RequestBody LoginRequest request) {
 		return authService.login(request);
-	}
-
-	@PostMapping("/register")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void register(@RequestBody RegisterRequest request) {
-		authService.register(request);
 	}
 
 	@PostMapping("/refresh")
